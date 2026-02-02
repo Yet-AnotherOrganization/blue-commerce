1f4e5417404844209520ea00d641fbcddf314fbc
frknecn3 <furkanduhannuman@gmail.com>
2026-02-03 02:33:29 +0300

    prisma updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export default prisma;
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Terminalde SQL sorgularını görmek için
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
