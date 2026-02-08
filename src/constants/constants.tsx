bdb34a5866f75b5a27490677f9c6df42fcb7e20a
frknecn3 <furkanduhannuman@gmail.com>
2026-02-08 15:43:52 +0300

    refactor(constants): standardize formatting and change class to interface for type definitions

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -7,77 +7,77 @@ import { MdContactSupport } from "react-icons/md";
 
 const Links = [
     {
-        name:'Login/Register',
-        href:'/login',
-        logo:<IoIosLogIn />
+        name: 'Login/Register',
+        href: '/login',
+        logo: <IoIosLogIn />
     },
     {
-        name:'Products',
-        href:'/',
-        logo:<FaShoppingBag />
+        name: 'Products',
+        href: '/',
+        logo: <FaShoppingBag />
 
     },
     {
-        name:'Upload',
-        href:'/product/create',
-        logo:<FaFileUpload/>
+        name: 'Upload',
+        href: '/product/create',
+        logo: <FaFileUpload />
     },
     {
-        name:'Contact',
-        href:'/Contact',
-        logo:<MdOutlineSupportAgent />
+        name: 'Contact',
+        href: '/Contact',
+        logo: <MdOutlineSupportAgent />
     },
     {
-        name:'About Us',
-        href:'/about',
-        logo:<MdContactSupport/>
+        name: 'About Us',
+        href: '/about',
+        logo: <MdContactSupport />
     },
-    
+
 ]
 
 export default Links
 
-export class ProductParams {
-    version:number
-    datePublished:string
-    id:string
-    name:string
-    photoURL:string
-    price:number
-    quantity:number
-    category:string
-    seller:string
-    desc:string
-    reviews:ReviewParams[]
-    stars:{
-        count:number
-        stars:number
+export interface ProductParams {
+    version: number
+    datePublished: string
+    id: string
+    name: string
+    photoURL: string
+    price: number
+    quantity: number
+    category: string
+    seller: string
+    desc: string
+    reviews: ReviewParams[]
+    stars: {
+        count: number
+        stars: number
     }
 }
 
-export class ReviewParams {
-    text:string
-    rating:1|2|3|4|5
-    sender:string
-    photoURL?:string
-    userObj?:User
+export interface ReviewParams {
+    text: string
+    rating: 1 | 2 | 3 | 4 | 5
+    sender: string
+    photoURL?: string
+    userObj?: User
 }
 
-export class User {
+export interface User {
     // constructor(userID?:string,photoURL?:string,allProducts?:string[],name?:string,cart?:string[],wishlist?:string[]){
     //     this.userID = userID||''
     //     this.photoURL=photoURL||''
     // }
 
-    userID:string
-    photoURL:string
-    allProducts:string[]
-    name:string
-    cart:string[]
-    wishlist:string[]
+    userID: string
+    photoURL: string
+    allProducts: string[]
+    name: string
+    cart: string[]
+    wishlist: string[]
 }
 
-export const categories:string[] = [
+export const categories: string[] = [
     '',
     'Entertainment',
     'Technology',
@@ -88,32 +88,32 @@ export const categories:string[] = [
 ]
 
 export type Option = {
-    value:string
-    label:string
+    value: string
+    label: string
 }
 
-export const SelectOptions:Option[] = categories.map((category)=>{return{value:category,label:category}})
+export const SelectOptions: Option[] = categories.map((category) => { return { value: category, label: category } })
 
 
 export const hotbarElements = [
     {
-        value:'trending',
-        label:'Trending'
+        value: 'trending',
+        label: 'Trending'
     },
     {
-        value:'whatsnew',
-        label:'Newest Products'
+        value: 'whatsnew',
+        label: 'Newest Products'
     },
     {
-        value:'offers',
-        label:'Special Offers'
+        value: 'offers',
+        label: 'Special Offers'
     },
     {
-        value:'promoted',
-        label:'Promoted Products'
+        value: 'promoted',
+        label: 'Promoted Products'
     },
     {
-        value:'fastdelivery',
-        label:'Same-day Delivery'
+        value: 'fastdelivery',
+        label: 'Same-day Delivery'
     },
 ]
