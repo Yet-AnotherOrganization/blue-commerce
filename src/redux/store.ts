2c029916b944ef2da2feb4dce5a4eec994bd1fa4
frknecn3 <furkanduhannuman@gmail.com>
2026-02-09 01:41:12 +0300

    refactor(store): simplify store configuration by using cartSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,20 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import {cartDisplayReducer, generalReducer} from './reducers'
-const rootReducer = combineReducers({
-    generalReducer,
-    cartDisplayReducer
-})
-export const store = () => {
-    return configureStore({
-        reducer:rootReducer
-    })
+import cartSlice from './slices/userSlice';
+
+
+export const makeStore = () => {
+
+    return configureStore(({
+        reducer: {
+            cart: cartSlice.reducer
+        }
+    }))
+
 }
+
+
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof store>
+export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
