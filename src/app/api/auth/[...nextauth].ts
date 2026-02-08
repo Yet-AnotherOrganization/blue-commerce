b8c8b3e53f340dcab7180f6051d2b4b5261d52de
frknecn3 <furkanduhannuman@gmail.com>
2026-02-08 03:25:11 +0300

    feat(nextauth): created prisma validation for auth

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth, { AuthOptions } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { prisma } from '../../../lib/prisma';
+import { User } from "../../../generated/prisma";
+import { compare, compareSync } from 'bcryptjs';
+
 
 export const authOptions: AuthOptions = {
     providers: [
@@ -9,6 +13,25 @@ export const authOptions: AuthOptions = {
                 email: { label: "email", type: "text", placeholder: "Email giriniz." },
                 password: { label: "password", type: "password" }
             },
+            async authorize(credentials, req): Promise<User | null> {
+                if (!credentials?.password || credentials.email) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.email
+                    }
+                })
+
+                if (!user || !user.password) return null
+
+                // need to compare hashes
+                const isPassValid = compareSync(credentials?.password, user.password);
+
+                // no match => wrong pass/email
+                if (!isPassValid) return null
+
+                return user
+            }
         }),
 
     ]
