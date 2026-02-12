35ad3ccad35b617853440a548139cfd408099786
frknecn3 <furkanduhannuman@gmail.com>
2026-02-12 16:08:42 +0300

    refactor(TotalComponent): update props to use cart array and improve total calculation

--- src/app/(pages)/cart/TotalComponent.tsx
+++ src/app/(pages)/cart/TotalComponent.tsx
@@ -1,13 +1,18 @@
+'use client'
 import '../../../components/css/index.css'
 import { ProductParams } from '../../../constants/constants';
+import { calculateTotalCost } from '../../../utils/clientOnlyUtils';
+import { CartItemWithProduct, ProductType } from '../../../utils/types';
 
-export const TotalComponent= ({calculateTotalCost, displayCart}:{calculateTotalCost:(a:ProductParams[])=>number,displayCart:ProductParams[]})=>{
-    return (
+type TotalComponentProps = { cart: CartItemWithProduct[] }
+
+export const TotalComponent = ({ cart }: TotalComponentProps) => {
+  return (
 
     <div className='total-component text-center p-2'>
-      <p>SELECTED PRODUCTS ({displayCart.length}) </p>
-      <p className='text-[3rem]'>{calculateTotalCost(displayCart)}</p>
-      <button disabled={displayCart.length>0?false:true} className='p-3 bg-blue-600 text-white rounded-xl hover:brightness-125 hover:translate-y-[-3px] transition-all'>CHECKOUT</button>
+      <p>SELECTED PRODUCTS ({cart.length}) </p>
+      <p className='text-[3rem]'>{calculateTotalCost(cart)}$</p>
+      <button disabled={cart.length > 0 ? false : true} className='p-3 bg-blue-600 text-white rounded-xl hover:brightness-125 hover:translate-y-[-3px] transition-all'>CHECKOUT</button>
     </div>
   );
-  }
+}
