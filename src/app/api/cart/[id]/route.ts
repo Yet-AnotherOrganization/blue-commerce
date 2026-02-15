736718da035be2fc8aec73afbe8a4cc4c5a4ded3
frknecn3 <furkanduhannuman@gmail.com>
2026-02-15 04:59:44 +0300

    feat(cart): implement session validation for GET cart request and update user access control

--- src/app/api/cart/[id]/route.ts
+++ src/app/api/cart/[id]/route.ts
@@ -1,15 +1,29 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 import { res } from "../../../../utils/serverUtils";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
+// REQUEST TO GET CART 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
-
     try {
+
+        const session = await getServerSession(authOptions);
+
+        if (!session || !session.user) {
+            return res(401, 'Please log in')
+        }
+
+        const requestedId = params.id;
+        const currentUserId = session.user.id;
+
+        if(requestedId !== currentUserId) return res(401, "You don't have access to this resource.")
+
         const id = params.id
 
         const cart = await prisma.cart.findUnique({
             where: {
-                userId: id
+                userId: requestedId
             },
             include: {
                 items: {
@@ -41,5 +55,19 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     catch (err) {
         console.log(err);
         return NextResponse.json({ success: false, message: "There was an error during fetching cart.", data: err }, { status: 500 })
+    }
+}
+
+// ADD TO CART
+
+export async function POST(req: Request) {
+    try {
+
+
+
+    }
+    catch (err) {
+        console.log(err)
+        return res(500, "An internal error has occurred", err)
     }
 }
