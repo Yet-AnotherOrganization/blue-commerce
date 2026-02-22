d4bccda45c5701edf33a9ebdf0a1d2c121f6f666
Furkan Ercan <furkanduhannuman@gmail.com>
2026-02-23 01:32:41 +0300

    feat(carousel): update CarouselComponent to use ProductWithCategory type and improve structure

--- src/components/CarouselComponent/index.tsx
+++ src/components/CarouselComponent/index.tsx
@@ -2,36 +2,34 @@
 import React from "react";
 import { ProductParams } from "../../constants/constants";
 import ArrowAnimation from "./ArrowAnimation";
+import { Product } from "../../generated/prisma";
+import { ProductWithCategory } from "../../types/product";
 
-const CarouselComponent = ({ product }: { product: ProductParams }) => {
+const CarouselComponent = ({ product }: { product: ProductWithCategory }):React.ReactNode => {
   const {
     price,
     category,
-    datePublished,
-    desc,
+    createdAt,
+    description,
     id,
     name,
-    photoURL,
-    quantity,
-    reviews,
-    seller,
-    stars,
-    version,
-  } = product;
+    imageUrl,
+    stock,
+} = product;
 
-  return (
-    <div className="p-1 bg-[#ffffff] flex flex-col justify-center items-center rounded-xl py-5 border h-auto shadow-md">
-        <div className="md:w-[20vw] md:h-[35vh] w-[80vw] h-[25vh] relative">
-          <a href={`/product/${product.id}`}>
-            <img src={photoURL} className="rounded-xl w-full h-full" alt="carousel photo" />
-            <ArrowAnimation />
-          </a>
-          
-        </div>
-        
-        <p className="text-center">{name}</p>
+return (
+  <div className="p-1 bg-[#ffffff] flex flex-col justify-center items-center rounded-xl py-5 border h-auto shadow-md">
+    <div className="relative bg-yellow-200">
+      <a href={`/product/${product.id}`}>
+        <img src={imageUrl} className="bg-red-400 w-[50vw] object-fill aspect-square lg:aspect-[5/3]" alt="carousel photo" />
+        <ArrowAnimation />
+      </a>
+
     </div>
-  );
+
+    <p className="text-center mt-2">{name}</p>
+  </div>
+);
 };
 
 export default CarouselComponent;
