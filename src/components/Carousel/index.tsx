d6beaa288b3a2348357dad61030077a3b11dfb96
Furkan Ercan <furkanduhannuman@gmail.com>
2026-02-23 01:32:34 +0300

    feat(carousel): update Slider component to use ProductWithCategory type and improve code formatting

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,34 +4,36 @@ import "react-multi-carousel/lib/styles.css";
 import React from 'react'
 import CarouselComponent from "../CarouselComponent";
 import { ProductParams } from "../../constants/constants";
+import { Product } from "../../generated/prisma";
+import { ProductWithCategory } from "../../types/product";
 
-const Slider = ({items,style}:{items:ProductParams[],style:string}) => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 1
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 1
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 1
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+const Slider = ({ items, style }: { items: ProductWithCategory[], style: string }) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
-    const random = Math.floor(Math.random()*items.length)
+  const random = Math.floor(Math.random() * items.length)
 
   return (
-  <Carousel className={style} responsive={responsive} slidesToSlide={1} renderButtonGroupOutside={true} infinite={true} autoPlay={true} autoPlaySpeed={2500}>
-    {items.slice(0,10).map((item,i)=>{return <CarouselComponent key={i} product={item} />})}
-  </Carousel>
+    <Carousel className={style} responsive={responsive} slidesToSlide={1} renderButtonGroupOutside={true} infinite={true} autoPlay={true} autoPlaySpeed={2500}>
+      {items.slice(0, 10).map((item, i) => { return <CarouselComponent key={i} product={item} /> })}
+    </Carousel>
   )
 }
 
