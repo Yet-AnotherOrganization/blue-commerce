917876a28535641ec34c931ec0740aaf8250c931
Furkan Ercan <furkanduhannuman@gmail.com>
2026-02-24 03:34:49 +0300

    fix(carousel): improve formatting and structure of CarouselComponent

--- src/components/CarouselComponent/index.tsx
+++ src/components/CarouselComponent/index.tsx
@@ -5,7 +5,7 @@ import ArrowAnimation from "./ArrowAnimation";
 import { Product } from "../../generated/prisma";
 import { ProductWithCategory } from "../../types/product";
 
-const CarouselComponent = ({ product }: { product: ProductWithCategory }):React.ReactNode => {
+const CarouselComponent = ({ product }: { product: ProductWithCategory }): React.ReactNode => {
   const {
     price,
     category,
@@ -15,21 +15,21 @@ const CarouselComponent = ({ product }: { product: ProductWithCategory }):React.
     name,
     imageUrl,
     stock,
-} = product;
+  } = product;
 
-return (
-  <div className="p-1 bg-[#ffffff] flex flex-col justify-center items-center rounded-xl py-5 border h-auto shadow-md">
-    <div className="relative bg-yellow-200">
-      <a href={`/product/${product.id}`}>
-        <img src={imageUrl} className="bg-red-400 w-[50vw] object-fill aspect-square lg:aspect-[5/3]" alt="carousel photo" />
-        <ArrowAnimation />
-      </a>
+  return (
+    <div className="p-4 bg-[#ffffff] flex flex-col justify-center items-center rounded-xl  border h-[40vh] shadow-md">
+      <div className="relative bg-yellow-200  aspect-square lg:aspect-video h-[90%]">
+        <a href={`/product/${product.id}`} className="">
+          <img src={imageUrl} className="w-full h-full object-fill" alt="carousel photo" />
+          <ArrowAnimation />
+        </a>
 
+      </div>
+
+      <p className="text-center mt-2">{name}</p>
     </div>
-
-    <p className="text-center mt-2">{name}</p>
-  </div>
-);
+  );
 };
 
 export default CarouselComponent;
