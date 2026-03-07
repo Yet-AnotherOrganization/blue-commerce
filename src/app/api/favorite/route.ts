b115ee023212a1598c084e370626ebd771a47942
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-07 10:48:13 +0300

    feat(favorite): enhance POST handler to validate product ID and add item to favorites

--- src/app/api/favorite/route.ts
+++ src/app/api/favorite/route.ts
@@ -1,15 +1,41 @@
 import { NextResponse } from "next/server";
 import { getUser, withErrorHandler } from "../../../utils/serverUtils"
+import APIError from "../../../types/api";
+import { prisma } from "../../../lib/prisma";
 
-const postHandler = async (req:Request,) => {
-    
-    await getUser();
+const postHandler = async (req: Request) => {
 
-    const body = await req.json();
+    const user = await getUser();
 
-    
+    if (!user) throw new APIError('No session found, please log in again.', 401, 'ERR_UNAUTHORIZED')
 
-    return NextResponse.json({success:true, message: 'Item successfully added to favorites', data: 'PLACEHOLDER'})
+    const { productId } = await req.json();
+
+    if (!productId) throw new APIError('Product ID was not given.', 400, 'ERR_NO_PRODUCTID')
+
+    const product = await prisma.product.findUnique({
+        where: {
+            id: productId
+        }
+    })
+
+    if (!product) throw new APIError('Product with given ID was not found', 404, 'ERR_NO_PRODUCT')
+
+    const favoriteItem = await prisma.favorite.upsert({
+        where: {
+            ownerId_productId: {
+                ownerId: user.id,
+                productId: productId
+            }
+        },
+        update: {},
+        create: {
+            ownerId: user.id,
+            productId
+        }
+    })
+
+    return NextResponse.json({ success: true, message: 'Item successfully added to favorites', data: favoriteItem })
 
 }
 
