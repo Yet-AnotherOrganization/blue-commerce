115c71f07f9b27b60849477052f00599e92d8096
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-08 03:24:26 +0300

    feat(favorite): implement DELETE handler to remove items from favorites

---
+++ src/app/api/favorite/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../lib/prisma";
+import APIError from "../../../../types/api";
+import { getUser, res, withErrorHandler } from "../../../../utils/serverUtils"
+
+type HandlerParams = {
+    params: { id: string }
+}
+
+const deleteHandler = async (req: Request, { params }:HandlerParams) => {
+
+    const user = await getUser();
+
+    const itemToDelete = await prisma.favorite.delete({
+        where:{
+            ownerId_productId:{
+                ownerId: user.id,
+                productId: params.id
+            }
+        }
+    })
+
+    if(!itemToDelete) throw new APIError('This product is not in your favorites', 404, 'ERR_FAVORITE_NOT_FOUND')
+
+    return res(204, 'Removed from favorites.')
+}
+
+export const DELETE = withErrorHandler(deleteHandler)
