4b125e7fa80a0466f01402a3a66ead9d7e9e4540
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-08 07:10:16 +0300

    fix(footer): add responsive width classes to footer images for better layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ const Footer = () => {
         <div className='text-[0.7rem] md:text-[1rem] p-4'>
             <p>2024 | All rights reserved.</p>
         </div>
-        <div className='flex md:flex-row flex-col gap-4 text-[0.5rem] md:text-[1rem] p-4'>
-          <img src="https://images.hepsiburada.net/assets/footer/bonus-card.svg" alt="" />
-          <img src="https://images.hepsiburada.net/assets/footer/maximum.svg" alt="" />
-          <img src="https://images.hepsiburada.net/assets/footer/world.svg" alt="" />
-          <img src="https://images.hepsiburada.net/assets/footer/ziraat.svg" alt="" />
-          <img src="https://images.hepsiburada.net/assets/footer/card-finans.svg" alt="" />
-          <img src="https://images.hepsiburada.net/assets/footer/axess.svg" alt="" />
+        <div className='flex sm:flex-row flex-col gap-4 text-[0.5rem] md:text-[1rem] p-4'>
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/bonus-card.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/maximum.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/world.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/ziraat.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/card-finans.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/axess.svg" alt="" />
         </div>
         <div className='p-4'>
             <p className='text-[0.7rem] md:text-[1rem] '>Made by Furkan Ercan</p>
