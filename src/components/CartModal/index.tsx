4a3e3b7faf9e7b3d80f7ca4648bd7083a92c405f
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-08 05:11:38 +0300

    feat(cartModal): enhance CartModal layout and functionality for better user experience

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -35,35 +35,70 @@ const CartModal = (props: Props) => {
 
         cartModalOpen &&
         <>
-            <div className='bg-white text-black border flex flex-col justify-between overflow-hidden border-black rounded-xl h-[40vh] w-[30vw] absolute bottom-[-42vh] z-[200000]'>
-                <div className='flex justify-between items-center text-xl font-semibold px-4 py-2 border-b'><div />
-                    <span>CART</span>
-                    <button className='bg-red-700 rounded-full cursor-pointer relative w-8 h-8' onClick={() => dispatch({ type: 'CLOSE_CART_MODAL' })}>
+            <>
+                <div className='bg-white text-black border flex-col justify-between overflow-hidden rounded-xl h-[40vh] w-[30vw] sm:w-[40vw] absolute bottom-[-42vh] z-[200000] hidden md:flex'>
+                    <div className='flex justify-between items-center text-xl font-semibold px-4 py-2 border-b'><div />
+                        <span>CART</span>
+                        <button className='bg-red-700 rounded-full cursor-pointer relative w-8 h-8' onClick={() => dispatch({ type: 'CLOSE_CART_MODAL' })}>
 
-                        <span
-                            className='text-white'
-                            style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>X</span>
-                    </button>
+                            <span
+                                className='text-white'
+                                style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>X</span>
+                        </button>
+                    </div>
+
+
+                    <div className='h-3/5 px-4 my-5 overflow-auto'>
+
+                        {cart?.map((item, i) => {
+
+                            return (
+
+                                <CartModalProduct product={item.product} key={i} />
+
+                            )
+                        })}
+
+                    </div>
+
+
+                    <div className='flex bg-gray-200 border-t-2 justify-between border-gray-300 px-4 py-2'>
+                        <button className='bg-orange-400 rounded-xl text-white p-2' onClick={() => { router.push('/cart') }}>GO TO CART</button>
+                        <span className='p-2 rounded-xl text-white bg-blue-300'>TOTAL PRICE: {calculateTotalCost(cart)}</span>
+                    </div>
                 </div>
+            </>
+            <div className='fixed md:hidden top-0 left-0 right-0 bottom-0 backdrop-blur-sm'>
+                <div className='bg-white text-black border flex flex-col justify-between overflow-hidden border-neutral-400 rounded-xl md:h-[40vh] md:w-[30vw] absolute md:bottom-[-42vh] z-[200000] left-1/2 top-1/2 md:right-0 translate-x-[-50%] translate-y-[-50%] md:translate-x-0 md:translate-y-0 w-[90vw] h-[90vh] '>
+                    <div className='flex justify-between items-center text-xl font-semibold px-4 py-2 border-b'><div />
+                        <span>CART</span>
+                        <button className='bg-red-700 rounded-full cursor-pointer relative w-8 h-8' onClick={() => dispatch({ type: 'CLOSE_CART_MODAL' })}>
+
+                            <span
+                                className='text-white'
+                                style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>X</span>
+                        </button>
+                    </div>
 
 
-                <div className='h-3/5 px-4 my-5 overflow-auto'>
+                    <div className='h-3/5 px-4 my-5 overflow-auto'>
 
-                    {cart?.map((item, i) => {
+                        {cart?.map((item, i) => {
 
-                        return (
+                            return (
 
-                            <CartModalProduct product={item.product} key={i} />
+                                <CartModalProduct product={item.product} key={i} />
 
-                        )
-                    })}
+                            )
+                        })}
 
-                </div>
+                    </div>
 
 
-                <div className='flex bg-gray-200 border-t-2 justify-between border-gray-300 px-4 py-2'>
-                    <button className='bg-orange-400 rounded-xl text-white p-2' onClick={() => { router.push('/cart')}}>GO TO CART</button>
-                    <span className='p-2 rounded-xl text-white bg-blue-300'>TOTAL PRICE: {calculateTotalCost(cart)}</span>
+                    <div className='flex bg-gray-200 border-t-2 justify-between border-gray-300 px-4 py-2'>
+                        <button className='bg-orange-400 rounded-xl text-white p-2' onClick={() => { router.push('/cart') }}>GO TO CART</button>
+                        <span className='p-2 rounded-xl text-white bg-blue-300'>TOTAL PRICE: {calculateTotalCost(cart)}</span>
+                    </div>
                 </div>
             </div>
         </>
