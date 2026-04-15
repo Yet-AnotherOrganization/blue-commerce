77219cad88530211a0a3d998687cfe5788aedf0f
Furkan Ercan <furkanduhannuman@gmail.com>
2026-04-15 14:19:08 +0300

    feat(admin): update AdminPage to include ProductsTable component

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,13 +1,20 @@
 'use client'
 import AdminSidebar from '@/components/Admin/AdminSidebar'
+import ProductsTable from '@/components/Admin/ProductsTable'
+import { prisma } from '@/lib/prisma'
 import React from 'react'
 
 type Props = {}
 
 const AdminPage = (props: Props) => {
-  return (
-        <div className='flex-[4] flex justify-center'>another div</div>
-  )
+
+    
+
+    return (
+        <div className='flex-[4] flex justify-center'>
+            <ProductsTable />
+        </div>
+    )
 }
 
 export default AdminPage
