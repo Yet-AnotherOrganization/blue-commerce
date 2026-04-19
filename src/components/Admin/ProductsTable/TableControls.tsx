25bea9a4ac320ec4d880fb7fddc593f03bc33a70
frknecn3 <furkanduhannuman@gmail.com>
2026-04-19 18:14:54 +0300

    wip(TableControls): add necessary params, path and router for later use.

--- src/components/Admin/ProductsTable/TableControls.tsx
+++ src/components/Admin/ProductsTable/TableControls.tsx
@@ -1,10 +1,20 @@
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
 
 const TableControls = (props: Props) => {
+
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const sParams = useSearchParams();
+    
   return (
-    <div className=''>TableControls</div>
+    <div className='mx-auto'>
+        {pathname}
+    </div>
   )
 }
 
