e9a86f8c61e0cdcd73e9ccd96b600bf0ca55b3a7
frknecn3 <furkanduhannuman@gmail.com>
2026-04-19 18:07:47 +0300

    feat(admin): add TableControls component to AdminPage

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,6 +1,6 @@
-'use client'
 import AdminSidebar from '@/components/Admin/AdminSidebar'
 import ProductsTable from '@/components/Admin/ProductsTable'
+import TableControls from '@/components/Admin/ProductsTable/TableControls'
 import { prisma } from '@/lib/prisma'
 import React from 'react'
 
@@ -11,8 +11,9 @@ const AdminPage = (props: Props) => {
     
 
     return (
-        <div className='flex-[4] flex justify-center'>
+        <div className='flex-[4] flex flex-col justify-center'>
             <ProductsTable />
+            <TableControls />
         </div>
     )
 }
---
+++ src/components/Admin/ProductsTable/TableControls.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TableControls = (props: Props) => {
+  return (
+    <div className=''>TableControls</div>
+  )
+}
+
+export default TableControls
