3b9494b492ec0a3f853a722766839aa14fd3e1dd
frknecn3 <furkanduhannuman@gmail.com>
2026-04-20 11:56:23 +0300

    feat(clientOnlyUtils): add changePage function for pagination handling

--- src/utils/clientOnlyUtils.tsx
+++ src/utils/clientOnlyUtils.tsx
@@ -1,6 +1,8 @@
 
+import { ReadonlyURLSearchParams } from "next/navigation";
 import { ProductParams } from "../constants/constants";
 import { CartItemWithProduct } from "../types/product";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export const getActiveUserFromStorage = (): void => {
     if (typeof window !== 'undefined') {
@@ -26,7 +28,18 @@ export const calculateTotalCost =
             // console.log(item.quantity)
         })
 
-        if(Number.isNaN(price)) console.log(price)
+        if (Number.isNaN(price)) console.log(price)
 
         return Number(price.toFixed(2));
     };
+
+
+export const changePage = (searchParams: ReadonlyURLSearchParams, p: number, router:AppRouterInstance, pathname:string) => {
+    const params = new URLSearchParams(searchParams.toString())
+    const currentPage = Number(params.get('page'))
+
+    const newPage = currentPage + p >= 1 ? currentPage + p : 1
+    params.set('page', String(newPage))
+    router.replace(`${pathname}?${params.toString()}`)
+
+}
