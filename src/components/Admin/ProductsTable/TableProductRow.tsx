f0e97e588a16dcfd9d87f5f0ca3bfe2171e09e6d
frknecn3 <furkanduhannuman@gmail.com>
2026-04-20 22:34:12 +0300

    feat(TableProductRow): add product row component with activation and deletion functionality

---
+++ src/components/Admin/ProductsTable/TableProductRow.tsx
@@ -0,0 +1,58 @@
+import { activateProduct, deleteProduct } from '@/app/actions/productActions'
+import { Product } from '@/generated/prisma'
+import React, { Dispatch } from 'react'
+import { FaCircle, FaTrash } from 'react-icons/fa'
+import { TiTick } from 'react-icons/ti'
+
+type Props = {
+    item: Product,
+}
+
+const TableProductRow = ({ item}: Props) => {
+
+    
+    return (
+        <tr className='border-y'>
+            <td className='px-4'>
+                <img src={item.imageUrl} className='w-12 h-12' alt="" />
+            </td>
+            <td>{item.name}</td>
+            <td>
+                <FaCircle color={item.status == "ACTIVE" ? "lightgreen" : item.status == "DRAFT" ? 'orange' : "red"} />
+            </td>
+            <td>{Number(item.price)}</td>
+            <td>{item.stock}</td>
+            <td>
+                {
+                    item.status == "ACTIVE" ?
+                        <button onClick={async () => {
+                            const confirmed = window.confirm('Are you sure you want to delete this product?');
+
+                            if (confirmed) {
+                                const res = await deleteProduct(item.id)
+                                if (!res?.success) {
+                                    alert(res?.message)
+                                }
+                            }
+                        }}>
+                            <FaTrash />
+                        </button> :
+                        <button onClick={async () => {
+                            const confirmed = window.confirm('Are you sure you want to activate this product for listing?');
+
+                            if (confirmed) {
+                                const res = await activateProduct(item.id)
+                                if (!res?.success) {
+                                    alert(res?.message)
+                                }
+                            }
+                        }}>
+                            <TiTick />
+                        </button>
+                }
+            </td>
+        </tr>
+    )
+}
+
+export default TableProductRow
