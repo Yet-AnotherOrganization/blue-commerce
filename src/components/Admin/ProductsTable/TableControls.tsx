9736ce39bdd27ae30d052ea67731b8f48c7771f4
frknecn3 <furkanduhannuman@gmail.com>
2026-04-20 12:38:00 +0300

    feat(TableControls): enhance pagination buttons with icons and improve layout

--- src/components/Admin/ProductsTable/TableControls.tsx
+++ src/components/Admin/ProductsTable/TableControls.tsx
@@ -2,6 +2,7 @@
 import { changePage } from '@/utils/clientOnlyUtils'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React, { Suspense } from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 type Props = {
   totalAmount: number
@@ -16,21 +17,21 @@ const TableControls = ({ totalAmount }: Props) => {
   const page = searchParams.get('page')
   const limit = searchParams.get('limit')
 
-
-
   return (
     <Suspense>
       <div className='flex-col w-full items-center justify-center mb-4'>
-        <div className='mx-auto justify-center flex gap-4 text-2xl '>
+        <div className='mx-auto justify-center flex gap-4 text-2xl my-5'>
           <button
             onClick={() => changePage(searchParams, -1, router, pathname)}
             disabled={Number(page) == 1}
             className={`${Number(page) == 1 && 'text-gray-200'}`}
-          >{'<-'}</button>
-          <span></span>
+          ><FaArrowLeft /></button>
+          <span>{page}</span>
           <button
+            disabled={totalAmount / Number(limit) <= Number(page)}
+            className={`${totalAmount / Number(limit) <= Number(page) && 'text-gray-200'}`}
             onClick={() => changePage(searchParams, 1, router, pathname)}
-          >{'->'}</button>
+          ><FaArrowRight /></button>
         </div>
 
         {/* limit */}
@@ -41,6 +42,7 @@ const TableControls = ({ totalAmount }: Props) => {
               console.log("mougs")
               const params = new URLSearchParams(searchParams.toString())
               params.set('limit', e.currentTarget.value)
+              params.set('page', "1")
               router.replace(`${pathname}?${params.toString()}`)
             }}
           >
