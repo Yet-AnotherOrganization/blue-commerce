b3b586d1d2c09bfbe764c16742af2eaa81e829ff
frknecn3 <furkanduhannuman@gmail.com>
2026-04-20 22:34:00 +0300

    feat(ProductsTable): implement sorting functionality for product columns

--- src/components/Admin/ProductsTable/index.tsx
+++ src/components/Admin/ProductsTable/index.tsx
@@ -4,8 +4,10 @@ import { Product, User } from '@/generated/prisma';
 import { prisma } from '@/lib/prisma';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { FaCircle, FaTrash } from 'react-icons/fa';
+import { FaArrowDown, FaArrowUp, FaCircle, FaTrash } from 'react-icons/fa';
 import { TiTick } from "react-icons/ti";
+import TableProductRow from './TableProductRow';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 type Props = {
     // fix any
     data: Product[];
@@ -14,74 +16,98 @@ type Props = {
 const ProductsTable = ({ data }: Props) => {
 
     const [page, setPage] = useState(1);
-    const limit = 20;
-    const [prods, setProds] = useState([])
+    const [sort, setSort] = useState<{ key: string, order: string }>({ key: '', order: 'asc' })
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
 
     useEffect(() => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set('sort', sort.key)
+        params.set('order', sort.order)
+        if (sort.key == '') params.delete('sort');
+        if (sort.order == '') params.delete('order');
+        router.replace(`${pathname}?${params.toString()}`)
 
-    }, [page])
+    }, [sort])
+
+    const reqSort = (key: string) => {
+        let direction = 'asc';
+        let newKey = key;
+
+        if (key == sort.key && sort.order == 'asc') direction = 'desc'
+        else if (key == sort.key && sort.order == 'desc') {
+            newKey = '';
+            direction = '';
+        }
+        else direction = 'asc'
+
+        setSort({ key: newKey, order: direction })
+    }
+
+    const getSortIcon = (name: string) => {
+        if (sort.key !== name) return ''
+        return sort.order === 'asc' ? <FaArrowDown /> : <FaArrowUp />
+    }
 
     return (
-        <table className='border border-black flex-1 overflow-scroll mt-[2vh]'>
-            <thead className='border'>
-                <tr className='font-bold bg-blue-100 sticky top-0'>
-                    <td className='px-4 py-8'>#</td>
-                    <td>Name</td>
-                    <td>Status</td>
-                    <td>Price</td>
-                    <td>Stock</td>
-                    <td>Actions</td>
-                </tr>
-            </thead>
-            <tbody className='border'>
-                {
-                    data.map((item) => {
-                        return (
-                            <tr className='border-y'>
-                                <td className='px-4'>
-                                    <img src={item.imageUrl} className='w-12 h-12' alt="" />
-                                </td>
-                                <td>{item.name}</td>
-                                <td>
-                                    <FaCircle color={item.status == "ACTIVE" ? "lightgreen" : item.status == "DRAFT" ? 'orange' : "red"} />
-                                </td>
-                                <td>{Number(item.price)}</td>
-                                <td>{item.stock}</td>
-                                <td>
-                                    {
-                                        item.status == "ACTIVE" ?
-                                            <button onClick={async () => {
-                                                const confirmed = window.confirm('Are you sure you want to delete this product?');
+        <div className='overflow-y-scroll mt-[2vh]'>
+            <table className='border border-black  w-full'>
+                <thead className='border'>
+                    <tr className='font-bold bg-blue-100 sticky top-0'>
+                        <td className='px-4 py-8'>
 
-                                                if (confirmed) {
-                                                    const res = await deleteProduct(item.id)
-                                                    if (!res?.success) {
-                                                        alert(res?.message)
-                                                    }
-                                                }
-                                            }}>
-                                                <FaTrash />
-                                            </button> :
-                                            <button onClick={async () => {
-                                                const confirmed = window.confirm('Are you sure you want to activate this product for listing?');
+                        </td>
+                        <td className='' onClick={() => reqSort('name')}>
+                            <div className='inline-flex justify-center items-center gap-1'>
+                                Name
+                                <span className='w-4'>
+                                    {getSortIcon('name')}
+                                </span>
+                            </div>
 
-                                                if (confirmed) {
-                                                    const res = await activateProduct(item.id)
-                                                    if (!res?.success) {
-                                                        alert(res?.message)
-                                                    }
-                                                }
-                                            }}>
-                                                <TiTick />
-                                            </button>
-                                    }
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+                        </td>
+                        <td className='' onClick={() => reqSort('status')}>
+                            <div className='inline-flex justify-center items-center gap-1'>
+                                Status
+                                <span className='w-4'>
+                                    {getSortIcon('status')}
+                                </span>
+                            </div>
+                        </td>
+                        <td className='' onClick={() => reqSort('price')}>
+                            <div className='inline-flex justify-center items-center gap-1'>
+                                Price
+                                <span className='w-4'>
+                                    {getSortIcon('price')}
+                                </span>
+                            </div>
+
+                        </td>
+                        <td className='' onClick={() => reqSort('stock')}>
+                            <div className='inline-flex justify-center items-center gap-1'>
+                                Stock
+                                <span className='w-4'>
+                                    {getSortIcon('stock')}
+                                </span>
+                            </div>
+                        </td>
+                        <td>
+                            Actions
+                        </td>
+                    </tr>
+                </thead>
+                <tbody className='border overflow-scroll '>
+                    {
+                        data.map((item) => {
+                            return (
+                                <TableProductRow item={item} />
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
