7d3ea43e4bedf0e8f71258a2a46afc3ca0e942fe
frknecn3 <furkanduhannuman@gmail.com>
2026-04-21 21:20:04 +0300

    feat(CreateProductPage): add page for creating new products with category and store selection

---
+++ src/app/(pages)/admin/product/create/page.tsx
@@ -0,0 +1,23 @@
+
+import { Product } from '@/generated/prisma'
+import React, { useState } from 'react'
+import CreateProductForm from './form'
+import { prisma } from '@/lib/prisma'
+
+type Props = {}
+
+const CreateProductPage = async (props: Props) => {
+
+    const categories = await prisma.category.findMany({});
+
+    const stores = await prisma.store.findMany({})
+
+    return (
+
+        <div className='mx-[20vw] py-[5vh]'>
+            <CreateProductForm categories={categories} stores={stores} />
+        </div>
+    )
+}
+
+export default CreateProductPage
