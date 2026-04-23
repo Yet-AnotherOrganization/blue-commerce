c270df047c4a6f136c8b2917535a32f9f1a55888
frknecn3 <furkanduhannuman@gmail.com>
2026-04-23 17:03:13 +0300

    feat(prisma): extend PrismaClient with custom slugging logic for user, product, and store entities

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,6 +5,63 @@ import { PrismaClient } from '../generated/prisma/client'
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter }).$extends({
+    query: {
+        user: {
+            // intercept create operation for slugging manually
+            async create({ args, query }) {
+                if (args.data.name) {
+                    args.data.nameSlug = args.data.name.toLowerCase().replace(/\s+/g, '')
+                }
+
+                return query(args);
+            },
+
+            // intercept update/upsert ops
+            async update({ args, query }) {
+                if (args.data.name && typeof args.data.name === 'string') {
+                    args.data.nameSlug = args.data.name.toLocaleLowerCase().replace(/\s+/g, '')
+                }
+                return query(args);
+            }
+        },
+        product: {
+            // intercept create operation for slugging manually
+            async create({ args, query }) {
+                if (args.data.name) {
+                    args.data.nameSlug = args.data.name.toLowerCase().replace(/\s+/g, '')
+                }
+
+                return query(args);
+            },
+
+            // intercept update/upsert ops
+            async update({ args, query }) {
+                if (args.data.name && typeof args.data.name === 'string') {
+                    args.data.nameSlug = args.data.name.toLocaleLowerCase().replace(/\s+/g, '')
+                }
+                return query(args);
+            }
+        },
+        store: {
+            // intercept create operation for slugging manually
+            async create({ args, query }) {
+                if (args.data.storeName) {
+                    args.data.nameSlug = args.data.storeName.toLowerCase().replace(/\s+/g, '')
+                }
+
+                return query(args);
+            },
+
+            // intercept update/upsert ops
+            async update({ args, query }) {
+                if (args.data.storeName && typeof args.data.storeName === 'string') {
+                    args.data.nameSlug = args.data.storeName.toLocaleLowerCase().replace(/\s+/g, '')
+                }
+                return query(args);
+            }
+        }
+    }
+})
 
 export { prisma }
