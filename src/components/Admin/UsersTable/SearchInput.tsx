95b45f03aa6e73f94cb21af6a889c936a85d14bd
frknecn3 <furkanduhannuman@gmail.com>
2026-04-25 15:11:07 +0300

    feat(SearchInput): implement search input with dynamic routing and input sanitization

--- src/components/Admin/UsersTable/SearchInput.tsx
+++ src/components/Admin/UsersTable/SearchInput.tsx
@@ -1,15 +1,38 @@
-import React, { ChangeEvent } from 'react'
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 type Props = {}
 
 const SearchInput = (props: Props) => {
 
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const [input, setInput] = useState<string>('');
+
+    useEffect(() => { 
+
+        const params = new URLSearchParams(searchParams.toString());
+        params.set('q', input);
+
+        router.replace(`${pathname}?${params}`);
+
+    }, [input])
+
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const searchValue = e.currentTarget.value.replace(' ', '')
+        const searchValue = e.currentTarget.value
+        .replaceAll(' ', '')
+        .replaceAll('-', '')
+        .toLowerCase();
+
+        console.log("amogus")
+
+        setInput(searchValue);
     }
 
     return (
-        <div className='bg-red-400'>
+        <div className='bg-'>
             <input placeholder='Enter user name' onChange={handleInputChange} />
         </div>
     )
