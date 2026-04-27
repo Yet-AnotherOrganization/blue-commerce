6dbea57221427f78d1c19917fc05e67ae8a4e463
frknecn3 <furkanduhannuman@gmail.com>
2026-04-27 20:16:58 +0300

    feat(EditProductDetails): enhance product edit page with category and store selection, refactor input handling

--- src/app/(pages)/admin/product/[id]/EditProductDetails.tsx
+++ src/app/(pages)/admin/product/[id]/EditProductDetails.tsx
@@ -1,58 +1,106 @@
 'use client';
-import { Product } from '@/generated/prisma'
+import { Category, Product, Store } from '@/generated/prisma'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import ControlBar from './ControlBar'
 import { SerializedProduct } from '@/types/product';
+import SelectWithSearch from '@/components/Common/SelectWithSearch';
 
 type Props = {
-    product: SerializedProduct
+    product: SerializedProduct,
+    categories: Category[],
+    stores: Store[]
 }
 
-const EditProductDetails = ({ product }: Props) => {
+type ProductDetailInputProps = {
+    fieldKey: string,
+    placeholder: string,
+    value: string,
+    fn: (val: string) => void,
+}
+
+const ProductDetailInput = ({ fieldKey, value, placeholder, fn }: ProductDetailInputProps) => {
+    return (
+        <div className='flex flex-col'>
+            <div className='flex'>
+                <label className='block flex-1 font-semibold'>{fieldKey}:</label>
+                <input value={value} onChange={(e) => fn(e.currentTarget.value)} placeholder={`Enter ${placeholder}`} className='flex-1 inline-flex items-center gap-1' />
+            </div>
+        </div>
+    )
+}
+
+
+const EditProductDetails = ({ product, categories, stores }: Props) => {
 
     const [name, setName] = useState(product.name);
+    const [slug, setSlug] = useState(product.nameSlug);
+    const [category, setCategory] = useState<string>(categories.find((cat) => cat.id === product.categoryId)?.id || '');
+    const [store, setStore] = useState<string>(stores.find((store) => store.id === product.sellerId)?.id || '');
 
     return (
-        <div className='mx-[5vw] pt-[3vh] lg:mx-[15vw] flex flex-col'>
+        <form onSubmit={(e) => e.preventDefault()} className='mx-[5vw] pt-[3vh] lg:mx-[15vw] flex flex-col'>
             <div className='flex gap-4 pb-4'>
                 <Link className='hover:text-blue-400' href={`/admin/product`}>Products</Link>
                 &gt;
                 <Link className='hover:text-blue-400' href={`#`}>{product?.name}</Link>
             </div>
-            <div className='flex bg-neutral-50 shadow-md border rounded-xl p-[50px] relative  flex-wrap'>
-                <div className=' flex-1'>
+            <div className='flex bg-neutral-50 shadow-md border rounded-xl p-[50px] relative  flex-wrap flex-col lg:flex-row'>
+                <div className='flex-1'>
                     <img
                         src={product?.imageUrl}
                         alt=""
-                        className='rounded-md shadow-lg'
+                        className='rounded-md shadow-lg aspect-square max-w-[300px] m-auto mb-0 max-lg:mb-10'
                     />
                 </div>
-                <div className='flex-[2] flex flex-col border-l-2 ml-4'>
+                <div className='flex-[2] flex flex-col lg:border-l-2 md:ml-4'>
 
                     <ControlBar product={product} />
 
                     <h1 className='text-center text-xl font-semibold'>Product Details</h1>
                     <div className='flex pt-4 pl-8 flex-col text-lg  gap-2'>
-                        <div className='flex flex-col'>
-                            <div className='flex items-center'>
-                                <div className='flex-1'>Name:</div>
-                                <div className='flex-[3]'>
-                                    <input className='px-2 py-1 border-2 border-black w-full' type="text" placeholder={product.name} defaultValue={name} />
-                                </div>
-                            </div>
+                        <ProductDetailInput fieldKey='Name' value={name} fn={setName} placeholder='product name' />
+                        <div className='flex items-center'>
+                            <label htmlFor="" className='flex-1 font-semibold'>Category:</label>
+                            <SelectWithSearch classes='flex-1 block' id='category' items={categories.map((cat) => ({ label: cat.name, value: cat.id }))} onChange={setCategory} placeholder='Select new category...' />
                         </div>
-                        <div className='flex flex-col'>
-                            <div className='flex items-center'>
-                                <div className='flex-1'>Slug:</div>
-                                <div className='flex-[3]'>{product?.nameSlug}</div>
-                            </div>
+                        <div className='flex items-center'>
+                            <label htmlFor="" className='flex-1 font-semibold'>Category:</label>
+                            <SelectWithSearch classes='flex-1 block' id='category' items={stores.map((store) => ({ label: store.storeName, value: store.id }))} onChange={setStore} placeholder='Select seller store...' />
                         </div>
+                        {/* 
+                        <DetailRow fieldKey='Slug'>
+                            {product.nameSlug || ''}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Date Created'>
+                            {product.createdAt.toLocaleDateString()} - {product.createdAt.toLocaleTimeString()}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Last Updated'>
+                            {product.updatedAt.toLocaleDateString()} - {product.updatedAt.toLocaleTimeString()}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Status'>
+                            <FaCircle size={10} color={product.status === 'ACTIVE' ? 'lightgreen' : product.status === 'DRAFT' ? 'orange' : 'red'} />
+                            {product.status}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Category'>
+                            {product.category.name}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Stock'>
+                            {product.stock}
+                        </DetailRow>
+
+                        <DetailRow fieldKey='Seller'>
+                            {product.seller.storeName}
+                        </DetailRow> */}
                     </div>
                 </div>
             </div>
-
-        </div>
+        </form>
     )
 }
 
