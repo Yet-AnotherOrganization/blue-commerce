f98f1260ddc1be83c288ffddda66704bdb960027
frknecn3 <furkanduhannuman@gmail.com>
2026-05-01 21:15:49 +0300

    feat(SelectWithSearch): enhance modal handling and keyboard navigation for improved accessibility

--- src/components/Common/SelectWithSearch.tsx
+++ src/components/Common/SelectWithSearch.tsx
@@ -17,6 +17,8 @@ type Props = {
     defaultSelected?: string
 }
 
+
+
 const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange, classes, defaultSelected }: Props) => {
 
     const [modalOpen, setModalOpen] = useState(false);
@@ -24,14 +26,16 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
     const containerRef = useRef<HTMLDivElement>(null);
     const [searchQuery, setSearchQuery] = useState('');
     const [filteredItems, setFilteredItems] = useState<Item[]>(items);
+    const [activeStep, setActiveStep] = useState(0);
 
+    const outerInputRef = useRef<HTMLInputElement>(null);
     const innerInputRef = useRef<HTMLInputElement>(null);
 
     // * CLOSE MODAL WHEN CLICKED OUTSIDE
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
-                setModalOpen(false);
+                handleModalOpen(false);
             }
         }
 
@@ -53,7 +57,7 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
         const foundItem = items.find((item) => item.value === value)
         // console.log("found", foundItem)
         if (foundItem) setSelectedItem(foundItem)
-        setModalOpen(false);
+        handleModalOpen(false);
     };
 
     const handleInputChange = (value: string) => {
@@ -68,11 +72,7 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
 
         const cleanQuery = standardizeText(value);
 
-        console.log("debounced")
-
         const results = items.filter((item) => standardizeText(item.label).includes(cleanQuery))
-
-        console.log("res: ", results)
         return setFilteredItems(results);
     }, 300), [items])
 
@@ -89,12 +89,49 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
         setFilteredItems(items)
     }, [items])
 
+
+    const handleModalOpen = (state: boolean) => {
+        setModalOpen(state);
+    }
+
+
+    const handleOuterKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (['Enter', ' ', 'ArrowDown'].includes(e.key)) {
+            e.preventDefault();
+            handleModalOpen(true);
+        }
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        switch (e.key) {
+            case 'ArrowDown':
+                e.preventDefault();
+                setActiveStep((prev) =>
+                    prev < filteredItems.length - 1 ? prev + 1 : prev
+                );
+                break;
+            case 'ArrowUp':
+                e.preventDefault();
+                setActiveStep((prev) => (prev > 0 ? prev - 1 : prev));
+                break;
+            case 'Enter':
+                e.preventDefault();
+                if (filteredItems[activeStep]) {
+                    handleSelect(filteredItems[activeStep].value);
+                }
+                break;
+            case 'Escape':
+                handleModalOpen(false);
+                break;
+        }
+    };
+
     return (
-        <div className={`relative inline-block ${classes}`} ref={containerRef}>
-            <input className='block w-full border border-gray-400 rounded-md px-2 py-1' type="text" readOnly value={selectedItem?.label} placeholder={placeholder || 'Enter placeholder text'} onClick={() => setModalOpen(true)} />
-            <input id={id} name={id} readOnly className='hidden' value={selectedItem?.value} />
-            <div className={`absolute flex flex-col p-2 left-0 right-0 border rounded-md shadow-lg bg-white ${modalOpen ? 'block' : 'hidden'} z-[100]`} >
-                <div className='inline-flex mb-2 border-b-2 items-center justify-center bg-white px-1'>
+        <div className={`relative inline-block  ${classes}`} ref={containerRef}>
+            <input id={id} name={id} className='block w-full border border-gray-400 rounded-md px-2 py-1' type="text" readOnly value={selectedItem?.label} placeholder={placeholder || 'Enter placeholder text'} onClick={() => handleModalOpen(true)} onKeyDown={handleOuterKeyDown} role='combobox' ref={outerInputRef} aria-controls={`${id}-listbox`} aria-expanded={modalOpen} />
+            <input readOnly className='hidden' value={selectedItem?.value} />
+            <div className={`absolute flex max-h-[170px] overflow-y-auto flex-col px-2 pb-2 left-0 right-0 border rounded-md shadow-lg bg-white ${modalOpen ? 'block' : 'hidden'} z-[100]`} >
+                <div className='inline-flex sticky top-0 mb-2 border-b-2 items-center justify-center bg-white px-1'>
                     <input
                         ref={innerInputRef}
                         className='outline-none rounded-xl w-full'
@@ -102,10 +139,12 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
                         placeholder={searchPlaceholder || 'Search items'}
                         value={searchQuery}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e.currentTarget.value)}
+                        aria-activedescendant={`id-opt-${activeStep}`}
+                        onKeyDown={handleKeyDown}
                     />
                     <FaSearch />
                 </div>
-                <ul className='flex flex-col'>
+                <ul className='flex flex-col' role='listbox' id={`${id}-listbox`}>
                     {
                         filteredItems.length <= 0 ?
 
@@ -113,8 +152,16 @@ const SelectWithSearch = ({ id, placeholder, searchPlaceholder, items, onChange,
 
                             :
 
-                            filteredItems?.map((item) =>
-                                <li onClick={(e) => { handleSelect(e.currentTarget.dataset.value || ''); setModalOpen(false); setSearchQuery('') }} className='inline-flex cursor-pointer hover:bg-gray-200 transition-all items-center justify-between overflow-ellipsis border-b border-gray-100' key={item.value} data-value={item.value}>
+                            filteredItems?.map((item, index) =>
+                                <li
+
+                                    onClick={(e) => { handleSelect(e.currentTarget.dataset.value || ''); setSearchQuery('') }}
+                                    className={`inline-flex cursor-pointer hover:bg-gray-200 transition-all items-center justify-between overflow-ellipsis border-b border-gray-100 focus:outline-2 focus:outline-blue-500 ${activeStep === index ? 'bg-gray-200' : ''}`}
+                                    key={item.value} data-value={item.value}
+                                    role='option'
+                                    id={`${id}-opt-${index}`}
+                                    aria-selected={item.value === selectedItem?.value}
+                                >
                                     {item.label}
                                     {item.value === selectedItem?.value && <TiTick />}
                                 </li>)
