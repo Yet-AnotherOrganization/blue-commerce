ad99104baad0719e5391cec8fe68ade4ee9dd254
frknecn3 <furkanduhannuman@gmail.com>
2026-05-03 17:29:32 +0300

    fix(Footer): update copyright year to 2026 and comment out unused images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <footer style={{'zIndex':`9999999999`}} className='bottom-0 w-[100vw] h-auto p-1 md:p-8 text-white flex justify-between items-center md:h-[10vh] bg-blue-400 border-t-8 border-blue-100'>
         <div className='text-[0.7rem] md:text-[1rem] p-4'>
-            <p>2024 | All rights reserved.</p>
+            <p>2026 | All rights reserved.</p>
         </div>
         <div className='flex sm:flex-row flex-col gap-4 text-[0.5rem] md:text-[1rem] p-4'>
-          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/bonus-card.svg" alt="" />
+          {/* <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/bonus-card.svg" alt="" />
           <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/maximum.svg" alt="" />
           <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/world.svg" alt="" />
           <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/ziraat.svg" alt="" />
           <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/card-finans.svg" alt="" />
-          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/axess.svg" alt="" />
+          <img className='w-8 md:w-16' src="https://images.hepsiburada.net/assets/footer/axess.svg" alt="" /> */}
         </div>
         <div className='p-4'>
             <p className='text-[0.7rem] md:text-[1rem] '>Made by Furkan Ercan</p>
