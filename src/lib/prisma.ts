e5d93c941de1189704be7478ce395cccd5b76f0a
frknecn3 <furkanduhannuman@gmail.com>
2026-05-03 15:38:39 +0300

    BREAKING CHANGES(prisma): fixed Prisma reconnection-lag due to HMR reset every single time

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,58 +5,71 @@ import { slugify } from "@/utils/utils";
 
 const unwantedStrings = ['.', ',', "'", "-", "_", "~", ""]
 
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-const connectionString = `${process.env.DATABASE_URL}`
+const extendedClient = (baseClient: PrismaClient) => {
+    return baseClient.$extends({
+        query: {
+            user: {
+                // intercept create operation for slugging manually
+                async create({ args, query }) {
+                    if (args.data.name) args.data.nameSlug = slugify(args.data.name)
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter }).$extends({
-    query: {
-        user: {
-            // intercept create operation for slugging manually
-            async create({ args, query }) {
-                if (args.data.name) args.data.nameSlug = slugify(args.data.name)
+                    return query(args);
+                },
 
-                return query(args);
+                // intercept update/upsert ops
+                async update({ args, query }) {
+                    if (args.data.name)
+                        args.data.nameSlug = slugify(args.data.name?.toString())
+                    return query(args);
+                }
             },
+            product: {
+                // intercept create operation for slugging manually
+                async create({ args, query }) {
+                    if (args.data.name) args.data.nameSlug = slugify(args.data.name)
 
-            // intercept update/upsert ops
-            async update({ args, query }) {
-                if (args.data.name)
-                    args.data.nameSlug = slugify(args.data.name?.toString())
-                return query(args);
-            }
-        },
-        product: {
-            // intercept create operation for slugging manually
-            async create({ args, query }) {
-                if (args.data.name) args.data.nameSlug = slugify(args.data.name)
+                    return query(args);
+                },
 
-                return query(args);
+                // intercept update/upsert ops
+                async update({ args, query }) {
+                    if (args.data.name)
+                        args.data.nameSlug = slugify(args.data.name?.toString())
+                    return query(args);
+                }
             },
+            store: {
+                // intercept create operation for slugging manually
+                async create({ args, query }) {
+                    if (args.data.storeName) args.data.nameSlug = slugify(args.data.storeName)
 
-            // intercept update/upsert ops
-            async update({ args, query }) {
-                if (args.data.name)
-                    args.data.nameSlug = slugify(args.data.name?.toString())
-                return query(args);
-            }
-        },
-        store: {
-            // intercept create operation for slugging manually
-            async create({ args, query }) {
-                if (args.data.storeName) args.data.nameSlug = slugify(args.data.storeName)
+                    return query(args);
+                },
 
-                return query(args);
-            },
-
-            // intercept update/upsert ops
-            async update({ args, query }) {
-                if (args.data.storeName)
-                    args.data.nameSlug = slugify(args.data.storeName?.toString())
-                return query(args);
+                // intercept update/upsert ops
+                async update({ args, query }) {
+                    if (args.data.storeName)
+                        args.data.nameSlug = slugify(args.data.storeName?.toString())
+                    return query(args);
+                }
             }
         }
-    }
-})
+    })
+}
 
-export { prisma }
+type PrismaClientExtended = ReturnType<typeof extendedClient>;
+
+
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString })
+
+const baseClient = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = baseClient;
+
+export const prisma = extendedClient(baseClient);
