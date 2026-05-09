2a93ddea7ae980a646e4fd8b2ed3f0193b6f476b
frknecn3 <furkanduhannuman@gmail.com>
2026-05-09 03:39:04 +0300

    feat(ControlBar): add product activation and deletion functionality with confirmation prompts

--- src/app/(pages)/admin/product/[id]/ControlBar.tsx
+++ src/app/(pages)/admin/product/[id]/ControlBar.tsx
@@ -1,10 +1,13 @@
 'use client'
+import { activateProduct, hardDeleteProduct, softDeleteProduct } from '@/app/actions/productActions';
 import { Product } from '@/generated/prisma'
 import { SerializedProduct } from '@/types/product';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { FaArchive, FaEdit } from 'react-icons/fa'
 import { GiConfirmed } from "react-icons/gi";
+import { MdDeleteForever, MdPublish } from 'react-icons/md';
+import { toast } from 'sonner';
 
 type Props = {
     product: SerializedProduct
@@ -17,7 +20,7 @@ const ControlBar = ({ product }: Props) => {
     const router = useRouter();
     const pathname = usePathname();
 
-    const [editMode, setEditMode] = useState<'EDIT' | 'VIEW' | string>(sParams.get('mode') || 'VIEW');
+    const [editMode, setEditMode] = useState<'EDIT' | 'VIEW'>(sParams.get('mode') as ('EDIT' | 'VIEW') || 'VIEW');
 
     useEffect(() => {
 
@@ -26,6 +29,15 @@ const ControlBar = ({ product }: Props) => {
         router.replace(`${pathname}?${params}`);
 
     }, [editMode])
+
+
+    const handleHardDelete = async () => {
+        if (confirm('WARNING: Are you sure you want to delete this product permanently? This action is irreversible.')) {
+            const res = await hardDeleteProduct(product.id);
+            if(res.success) {router.replace('/admin/product'); toast.success('Product successfully deleted.')}
+            
+        }
+    }
 
     return (
         <div className='control-bar absolute top-0 right-0 bg-white p-2 m-1 rounded-md flex gap-2 shadow-md'>
@@ -37,7 +49,18 @@ const ControlBar = ({ product }: Props) => {
                     <button className='inline-flex items-center gap-1 bg-green-400 px-2 py-1 rounded text-white hover:scale-105 transition-all hover:bg-green-500 w-24 justify-center'
                         onClick={() => setEditMode('VIEW')}><GiConfirmed />Confirm</button>
             }
-            <button className='inline-flex items-center gap-1 bg-red-700 text-white px-2 py-1 rounded hover:scale-105 transition-all hover:bg-red-900'><FaArchive />Archive</button>
+            {
+                editMode === 'VIEW' && (product.status === 'ACTIVE' ?
+                    <button onClick={() => softDeleteProduct(product.id)} className='inline-flex items-center gap-1 bg-red-700 text-white px-2 py-1 rounded hover:scale-105 transition-all hover:bg-red-900'><FaArchive />Archive</button>
+                    :
+                    <button onClick={() => activateProduct(product.id)} className='inline-flex items-center gap-1 bg-green-600 text-white px-2 py-1 rounded hover:scale-105 transition-all hover:bg-green-700'><MdPublish />Publish</button>
+                )
+            }
+            {
+                product.status === 'ARCHIVED' &&
+                <button onClick={handleHardDelete} className='inline-flex items-center gap-1 bg-red-700 text-white px-2 py-1 rounded hover:scale-105 transition-all hover:bg-red-900'><MdDeleteForever />Delete</button>
+
+            }
         </div>
     )
 }
