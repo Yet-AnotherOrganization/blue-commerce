3ef69bbf9ffe463db40c315b73d4d9ec4932b280
frknecn3 <furkanduhannuman@gmail.com>
2026-05-13 02:14:29 +0300

    feat(AskModal): add AskModal component with basic structure and styling

---
+++ src/components/Common/AskModal.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const AskModal = (props: Props) => {
+    
+    return (
+        <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-10 bg-red-400'>
+
+        </div>
+    )
+}
+
+export default AskModal
