8fb641ef188c1c98e3ddfd7eb14c93db5281ce9f
frknecn3 <furkanduhannuman@gmail.com>
2026-05-14 18:15:27 +0300

    refactor(Sidebar): simplify imports and replace dispatch with useConfirm for modal handling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,10 @@
 'use client'
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { useState } from 'react'
 import Links from '../constants/constants'
-import { handleLogOut } from '../utils/clientOnlyUtils.tsx'
 import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { IoIosClose, IoIosLogIn, IoIosMenu } from 'react-icons/io'
-import { FaCross } from 'react-icons/fa'
-import { askGenericModal } from '@/redux/slices/uiSlice.ts'
-import { useAppDispatch } from '@/redux/hooks.ts'
+import { useConfirm } from '@/context/ConfirmContext.tsx'
 
 type SidebarProps = {
 }
@@ -16,14 +13,23 @@ const Sidebar = () => {
 
     const [sidebar, setSidebar] = useState(false);
     const user = useSession().data?.user
-    const dispatch = useAppDispatch();
+
+    const ask = useConfirm();
+
+    const handleBtnClick = async () => {
+
+        const res = await ask('Do you really want to do this?')
+
+        if (res) { alert('Modal accepted.') } else { alert('Modal Rejected') }
+
+    }
 
     return (
         <>
             <button className='text-2xl lg:text-[40px]' onClick={() => { setSidebar(true) }}><IoIosMenu /></button>
 
             <div style={{ 'zIndex': '40', 'right': `${sidebar ? '0' : '-60vw'}` }} className={`fixed top-0 pt-[10vh] bg-blue-500 text-white inline-block w-[60vw] lg:w-[15vw] h-[100vh] transition-all p-4`} >
-                <button onClick={()=>setSidebar(false)} className='text-6xl absolute top-6 right-6'><IoIosClose /></button>
+                <button onClick={() => setSidebar(false)} className='text-6xl absolute top-6 right-6'><IoIosClose /></button>
                 <ul className='flex gap-6 flex-col'>
                     {
                         !user && <li>
@@ -54,7 +60,7 @@ const Sidebar = () => {
                         </Link>
                     </li>}
 
-                    <button onClick={()=>dispatch(askGenericModal({text: 'Are you sure you want to archive this product?', title:'Archive Product'}))}>
+                    <button onClick={handleBtnClick}>
                         TEST MODAL
                     </button>
                 </ul>
