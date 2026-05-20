5e964973ff219a29838282602402567afff34d11
frknecn3 <furkanduhannuman@gmail.com>
2026-05-20 19:31:51 +0300

    refactor(PriceComponent): clean up code formatting and remove unused CSS import

--- src/components/PriceComponent/index.tsx
+++ src/components/PriceComponent/index.tsx
@@ -1,16 +1,15 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { ProductParams } from '../../constants/constants'
-import '../css/index.css'
 import { Product } from '../../generated/prisma'
 
-const PriceComponent = ({currentProduct}:{currentProduct:Product}) => {
+const PriceComponent = ({ currentProduct }: { currentProduct: Product }) => {
 
 
   return (
     <div className={`w-full z-30 flex flex-col text-center price-component`}>
-        <span className="text-[2.5rem] md:text-[3rem] font-semibold">${(currentProduct?.price).toString()}</span>
-        <span className="text-base md:text-[1.5rem] font-normal text-gray-600">Up to 12 installments</span>
+      <span className=" font-semibold">${(currentProduct?.price).toString()}</span>
+      <span className=" font-normal text-gray-600">Up to 12 installments</span>
     </div>
   )
 }
