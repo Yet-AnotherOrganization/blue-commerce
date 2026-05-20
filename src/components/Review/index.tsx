31ff86c9fd73f654996d16d68f77ca13a8a3195d
frknecn3 <furkanduhannuman@gmail.com>
2026-05-20 19:32:29 +0300

    refactor(Review): improve styling and adjust text sizes for better readability

--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -9,10 +9,10 @@ const Review = ({ i, user, review }: { i: number, user: User, review: ReviewType
 
 
   return (
-    <div className='bg-white relative rounded-xl py-4 mt-1 w-full h-full border border-gray-400'>
+    <div className='bg-white relative rounded-xl py-4 mt-1 w-full h-full border border-gray-300'>
 
       <div className="flex items-center justify-end absolute top-4 right-1 p-2 w-auto text-center">
-        <span className="flex items-center justify-center md:text-[1rem] lg:text-[1.5rem] text-yellow-400 mt-[-1rem]">{
+        <span className="flex items-center justify-center md:text-md lg:text-sm text-yellow-400 mt-[-1rem]">{
           [...Array(5)].map((_, index) => {
             return index < (review.rating || 3) ? <FaStar key={index} /> : <FaRegStar key={index} />;
           })
@@ -23,13 +23,13 @@ const Review = ({ i, user, review }: { i: number, user: User, review: ReviewType
 
       <div key={i} className="relative flex py-1 gap-2  items-center justify-center w-full">
 
-        <div className="flex flex-col items-center justify-center mx-5 w-[10vw]">
-          <Link href={`/profile/${user.id}`}><img src={user?.avatar || '/vercel.svg'} className="w-[6.8vw] h-[6.8vw] rounded-full" alt="" /></Link>
-          <span className="md:text-[1rem] lg:text-[1.5rem] font-semibold text-center">{user?.name}</span>
-        </div>
+        <Link href={`/profile/${user.id}`} className="flex flex-col items-center justify-center mx-5 w-[10vw]">
+          <div><img src={user?.avatar || '/vercel.svg'} className="w-10 h-10 rounded-full" alt="" /></div>
+          <span className="md:text-md lg:text-xs font-semibold text-center">{user?.name}</span>
+        </Link>
 
         <div className="w-full">
-          <p className="md:text-[1rem] lg:text-[1.5rem] p-1 overflow-auto">{review?.text}</p>
+          <p className="md:text-md lg:text-sm p-1 overflow-auto">{review?.text}</p>
         </div>
 
       </div>
