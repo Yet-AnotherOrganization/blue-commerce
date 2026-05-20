042716598e12cf2819c4ca4099c02edcdeb14292
frknecn3 <furkanduhannuman@gmail.com>
2026-05-20 19:32:19 +0300

    refactor(ProductButtons): streamline button functionality and improve state management

--- src/components/ProductButtons.tsx
+++ src/components/ProductButtons.tsx
@@ -1,54 +1,66 @@
 "use client";
 
-import { useDispatch } from "react-redux";
 import { getActiveUserFromStorage } from "../utils/clientOnlyUtils";
-import { addToCart, reloadCart } from "../utils/utils";
-import '../components/css/index.css'
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { addToCart } from "@/redux/slices/cartSlice";
+import { addToFavorites, removeFromFavorites } from "@/redux/slices/favoriteSlice";
 
-const ProductButtons = ({ product }: { product: string }) => {
-  const dispatch = useDispatch();
+const ProductButtons = ({ product, style }: { product: string, style?: string }) => {
+  const dispatch = useAppDispatch();
   const user = getActiveUserFromStorage();
-  const [isClicked,setIsClicked] = useState<boolean>(false)
+  const [isAddClicked, setIsAddClicked] = useState<boolean>(false)
+  const [isFavClicked, setIsFavClicked] = useState<boolean>(false)
   const hasWindow = typeof window !== "undefined";
   const width = hasWindow ? window.innerWidth : null;
-  console.log(width)
+  const favorites = useAppSelector(state => state.favoriteReducer.ids )
+  const isFavorited = favorites.includes(product);
 
   return (
-    <div className={`product-buttons w-full h-[10vh] md:text-[1rem] lg:text-[1.5rem] flex-grow text-white flex lg:flex-row justify-center items-center gap-[3rem]`}>
+    <div className={`${style}`}>
       <button
-        className="bg-green-500 shadow-md relative p-2 lg:p-4 m-4 flex justify-center items-center rounded-xl hover:translate-y-[-5px] transition-all"
-        onClick={() => {
-          try {
-            addToCart(user.uid, product), reloadCart(user.uid, dispatch);
-            setIsClicked(true)
-          } catch (err) {
-            console.log(err);
-          }
-        }}
+        className="bg-green-500 shadow-md relative p-2 lg:p-4 m-4 flex flex-1 justify-center items-center rounded-xl hover:translate-y-[-5px] transition-all"
+        onClick={async () => await dispatch(addToCart({ productId: product }))}
       >
-              <motion.span
-        onClick={()=>{setIsClicked(true);!isClicked?setTimeout(() => setIsClicked(false), 2000):'';}}
-        whileTap={{ scale: 0.9 }}
-        animate={{y:isClicked?30:0,opacity:isClicked?0:100}}
-        transition={{ duration: 0.3 }}
-      >
-        ADD TO CART
-      </motion.span>
-      
-      <motion.span
-        onClick={()=>{setIsClicked(true);setTimeout(() => setIsClicked(false), 2000);}}
-        whileTap={{ scale: 0.9 }}
-        animate={{y:isClicked?0:-30,opacity:isClicked?100:0}}
-        transition={{ duration: 0.3 }}
-        className="absolute"
-      >ITEM ADDED</motion.span>
+        <motion.span
+          onClick={() => { setIsAddClicked(true); !isAddClicked ? setTimeout(() => setIsAddClicked(false), 2000) : ''; }}
+          whileTap={{ scale: 0.9 }}
+          animate={{ y: isAddClicked ? 30 : 0, opacity: isAddClicked ? 0 : 100 }}
+          transition={{ duration: 0.3 }}
+        >
+          ADD TO CART
+        </motion.span>
+
+        <motion.span
+          onClick={() => { setIsAddClicked(true); setTimeout(() => setIsAddClicked(false), 2000); }}
+          whileTap={{ scale: 0.9 }}
+          animate={{ y: isAddClicked ? 0 : -30, opacity: isAddClicked ? 100 : 0 }}
+          transition={{ duration: 0.3 }}
+          className="absolute"
+        >ITEM ADDED</motion.span>
       </button>
 
 
-      <button className="bg-blue-500 shadow-md p-2 lg:p-4 m-4 rounded-xl flex justify-center items-center hover:translate-y-[-5px] text-center transition-all">
-        <span>ADD TO WISHLIST</span>
+      <button 
+      onClick={async () => dispatch(!isFavorited ? addToFavorites(product) : removeFromFavorites(product))}
+      className="bg-blue-500 shadow-md p-2 lg:p-4 m-4 rounded-xl flex flex-1 justify-center items-center hover:translate-y-[-5px] text-center transition-all">
+        <motion.span
+          onClick={() => { setIsFavClicked(true); !isFavClicked ? setTimeout(() => setIsFavClicked(false), 2000) : ''; }}
+          whileTap={{ scale: 0.9 }}
+          animate={{ y: isFavClicked ? 30 : 0, opacity: isFavClicked ? 0 : 100 }}
+          transition={{ duration: 0.3 }}
+        >
+          {!isFavorited ? 'ADD TO WISHLIST' : 'REMOVE FROM WISHLIST'}
+        </motion.span>
+
+        <motion.span
+          onClick={() => { setIsFavClicked(true); setTimeout(() => setIsFavClicked(false), 2000); }}
+          whileTap={{ scale: 0.9 }}
+          animate={{ y: isFavClicked ? 0 : -30, opacity: isFavClicked ? 100 : 0 }}
+          transition={{ duration: 0.3 }}
+          className="absolute"
+        >{!isFavorited ? 'ITEM ADDED TO WISHLIST' : 'ITEM REMOVED FROM WISHLIST'}</motion.span>
       </button>
 
     </div>
