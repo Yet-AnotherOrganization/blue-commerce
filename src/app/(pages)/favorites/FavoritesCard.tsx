64364f52c420980a7fdf8e731c4d115946320809
frknecn3 <furkanduhannuman@gmail.com>
2026-05-20 20:58:08 +0300

    feat(Favorites): add FavoriteCard component for displaying favorite items with cart functionality

---
+++ src/app/(pages)/favorites/FavoritesCard.tsx
@@ -0,0 +1,82 @@
+'use client';
+import { useConfirm } from "@/context/ConfirmContext";
+import { useAppDispatch } from "@/redux/hooks";
+import { addToCart } from "@/redux/slices/cartSlice";
+import { removeFromFavorites } from "@/redux/slices/favoriteSlice";
+import { SerializedFavorite } from "@/types/product";
+import Link from "next/link";
+import { FaTrash } from "react-icons/fa";
+import { toast } from "sonner";
+
+const FavoriteCard = ({ fav }: { fav: SerializedFavorite }) => {
+    console.log(fav)
+
+    const stockText = fav.item.stock > 10 ? 'IN STOCK' :
+        fav.item.stock > 0 ? 'LOW STOCK' :
+            'OUT OF STOCK'
+    const stockColor = fav.item.stock > 10 ? 'text-green-400' :
+        fav.item.stock > 0 ? 'text-orange-400' :
+            'text-red-600'
+    
+
+    const dispatch = useAppDispatch();
+
+    const ask = useConfirm();
+
+    const handleRemoveFromCart = async () => {
+        const confirmed = await ask('Are you sure you want to remove this item from your favorites?');
+
+        if (confirmed) {
+            const res = await dispatch(removeFromFavorites(fav.productId));
+
+            if(res.meta.requestStatus == 'fulfilled') {
+                return toast.success('Item successfully removed from wishlist.')
+            }
+
+            return toast.error(`ERROR: ${res.payload || 'Unknown error.'}`)
+        }
+    }
+
+    const formatter = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+
+    return (
+        <div
+            className='xl:max-w-[300px] p-2 lg:p-4 shadow-md border rounded-xl flex flex-col'
+        >
+            <div className='w-full'>
+                <Link href={`/product/${fav.item.id}`}>
+                    <img src={fav.item.imageUrl} className='aspect-square rounded-md' alt="" />
+                </Link>
+            </div>
+            <div className='text-content px-2'>
+                <div className='py-4 h-max'>
+                    <h1 className='text-sm lg:text-lg font-semibold line-clamp-1' title={fav.item.name}>{fav.item.name}</h1>
+                </div>
+                <div className='flex justify-between max-lg:flex-col'>
+                    <span className={`font-semibold text-xl ${stockColor}`}>
+                        {
+                            stockText
+                        }
+                    </span>
+                    <span className='font-bold text-xl'>
+                        ${formatter.format(fav.item.price)}
+                    </span>
+                </div>
+                <div className='btns flex gap-4 pt-4 max-lg:flex-col'>
+                    <button
+                        onClick={() => dispatch(addToCart({ productId: fav.productId }))}
+                        className='block py-2 text-center flex-[3] bg-green-400 rounded-xl text-white hover:bg-green-500 transition-all text-lg'>
+                        ADD TO CART
+                    </button>
+                    <button
+                        onClick={handleRemoveFromCart}
+                        className='inline-flex py-3 flex-1 justify-center items-center bg-red-400 text-white rounded-xl hover:bg-red-500 transition-all'>
+                        <FaTrash />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FavoriteCard;
--- src/app/(pages)/favorites/FavoritesGrid.tsx
+++ src/app/(pages)/favorites/FavoritesGrid.tsx
@@ -1,65 +1,33 @@
+import { useAppDispatch } from '@/redux/hooks'
+import { addToCart } from '@/redux/slices/cartSlice'
 import { SerializedFavorite } from '@/types/product'
 import Link from 'next/link'
 import React from 'react'
-import { FaTrash } from 'react-icons/fa'
+import { FaShoppingBasket, FaTrash } from 'react-icons/fa'
+import FavoriteCard from './FavoritesCard'
 
 type Props = {
     favorites: SerializedFavorite[]
 }
 
 
-const FavoriteCard = ({ fav }: { fav: SerializedFavorite }) => {
 
-    const stockText = fav.item.stock > 10 ? 'IN STOCK' :
-        fav.item.stock > 0 ? 'LOW STOCK' :
-            'OUT OF STOCK'
-    const stockColor = fav.item.stock > 10 ? 'text-green-400' :
-        fav.item.stock > 0 ? 'text-orange-400' :
-            'text-red-600'
-
-    return (
-        <div
-            className='xl:max-w-[300px] p-2 lg:p-4 shadow-md border rounded-xl flex flex-col'
-        >
-            <div className='w-full'>
-                <Link href={`/product/${fav.item.id}`}>
-                    <img src={fav.item.imageUrl} className='aspect-square rounded-md' alt="" />
-                </Link>
-            </div>
-            <div className='text-content px-2'>
-                <div className='py-4 h-max'>
-                    <h1 className='text-sm lg:text-lg font-semibold line-clamp-1' title={fav.item.name}>{fav.item.name}</h1>
-                </div>
-                <div className='flex justify-between max-lg:flex-col'>
-                    <span className={`font-semibold text-xl ${stockColor}`}>
-                        {
-                            stockText
-                        }
-                    </span>
-                    <span className='font-bold text-xl'>
-                        ${fav.item.price.toLocaleString()}
-                    </span>
-                </div>
-                <div className='btns flex gap-4 pt-4 max-lg:flex-col'>
-                    <button className='block py-2 text-center flex-[3] bg-green-400 rounded-xl text-white hover:bg-green-500 transition-all text-lg'>
-                        ADD TO CART
-                    </button>
-                    <button className='inline-flex py-3 flex-1 justify-center items-center bg-red-400 text-white rounded-xl hover:bg-red-500 transition-all'>
-                        <FaTrash />
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
-}
 
 const FavoritesGrid = ({ favorites }: Props) => {
     return (
         <div className='mx-auto w-[90vw] shadow-md border m-8 rounded-xl'>
-            <h1 className='text-center text-4xl py-8 font-bold tracking-wider'>Your Favorited Items</h1>
+            <h1 className='text-center text-3xl md:text-2xl py-8 font-bold tracking-wider'>Your Favorited Items</h1>
             <div className='p-4 px-4 lg:px-20 gap-y-4 gap-x-4 grid max-md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]  max-lg:grid-cols-[repeat(auto-fill,minmax(350px,1fr))] grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-items-center justify-center'>
                 {
                     favorites.map((fav) => <FavoriteCard key={fav.id} fav={fav} />)
+                }
+                {
+                    favorites.length == 0 &&
+                    <div className='text-center text-xl pb-10 gap-10 flex flex-col items-center'>
+                        You don't have any products in your favorites list.
+
+                        <Link href={'/'} className='bg-blue-400 text-white py-4 px-8 rounded-xl hover:bg-blue-600 transition-all inline-flex items-center justify-center'>Back To Shopping <FaShoppingBasket /></Link>
+                    </div>
                 }
             </div>
         </div>
