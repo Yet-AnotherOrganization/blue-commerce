26ee443795a84b3b7229693ccbf0f1ddc98da3a7
frknecn3 <furkanduhannuman@gmail.com>
2026-05-20 21:24:11 +0300

    refactor(PriceComponent): update ProductButtons styling for better responsiveness

--- src/components/PriceComponent/index.tsx
+++ src/components/PriceComponent/index.tsx
@@ -10,7 +10,7 @@ const PriceComponent = ({ currentProduct }: { currentProduct: Product }) => {
   return (
     <div className={`w-full z-30 flex flex-col text-center price-component bg-white bottom-0 py-8`}>
       <span className=" font-semibold">${(currentProduct?.price).toString()}</span>
-      <ProductButtons product={currentProduct.id} style='flex text-white font-medium' />
+      <ProductButtons product={currentProduct.id} style='max-lg:flex hidden text-white font-medium' />
     </div>
   )
 }
