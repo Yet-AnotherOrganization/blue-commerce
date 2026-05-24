9ec020194524b3cabec46db2d46f6259243ed9dc
frknecn3 <furkanduhannuman@gmail.com>
2026-05-24 23:41:45 +0300

    style(LoginForm): enhance form styling with centered layout and button styles

--- src/app/(pages)/(auth)/login/LoginForm.tsx
+++ src/app/(pages)/(auth)/login/LoginForm.tsx
@@ -36,11 +36,11 @@ const LoginForm = (props: Props): React.ReactElement => {
     }
 
     return (
-        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-            <input className='border p-4 rounded-md' type="text" placeholder='Email' onChange={(e: ChangeEvent<HTMLInputElement>) => { setData({ ...data, email: e.target.value }); console.log(data) }} />
-            <input className='border p-4 rounded-md' type="password" placeholder='Password' onChange={(e: ChangeEvent<HTMLInputElement>) => { setData({ ...data, password: e.target.value }); console.log(data) }} />
-            <button>Login</button>
-        </form>
+            <form className='flex flex-col gap-4 items-center' onSubmit={handleSubmit}>
+                <input className='border p-4 rounded-md' type="text" placeholder='Email' onChange={(e: ChangeEvent<HTMLInputElement>) => { setData({ ...data, email: e.target.value }); console.log(data) }} />
+                <input className='border p-4 rounded-md' type="password" placeholder='Password' onChange={(e: ChangeEvent<HTMLInputElement>) => { setData({ ...data, password: e.target.value }); console.log(data) }} />
+                <button className='bg-blue-600 text-white px-4 py-2 rounded-md'>Login</button>
+            </form>
     )
 }
 
