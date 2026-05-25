acd1d57b2e27afb2883d49c7b04413c11f5129c6
frknecn3 <furkanduhannuman@gmail.com>
2026-05-25 23:55:26 +0300

    feat(checkout): update CheckoutPage to display order success message and order number

--- src/app/(pages)/checkout/[id]/page.tsx
+++ src/app/(pages)/checkout/[id]/page.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    params: { id: string }
+}
 
-const CheckoutPage = (props: Props) => {
+const CheckoutPage = ({ params }: Props) => {
     return (
-        <div>CheckoutPage</div>
+        <div className='flex flex-col text-lg lg:h-[50vh] justify-center items-center gap-4'>
+            <h1 className='text-4xl font-semibold'>SUCCESS</h1>
+            <p><span className='text-emerald-600'>ORDER NO:</span> {params.id}</p>
+            <p>We've received your order and it will shortly be delivered for cargo.</p>
+            <p className='text-blue-400 hover:text-blue-600'><Link href={'/orders'}>Check the status of your orders here</Link></p>
+        </div>
     )
 }
 
